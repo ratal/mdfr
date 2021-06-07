19eb12a797084c94b0bc71d265f8dfd3fb30299b
«ratal» <ratal@ratal.org>
2021-06-07 22:31:02 +0200

    changed benchmark from info to reader

--- benches/mdf_benchmark.rs
+++ benches/mdf_benchmark.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use mdfr::mdfinfo::mdfinfo;
+use mdfr::{mdfinfo::mdfinfo, mdfreader::mdfreader};
 use std::process::Command;
 
 fn python_launch() {
@@ -15,7 +15,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("ExtremeInfo");
     let file_name = "/home/ratal/workspace/mdfreader/mdfreader/tests/MDF4/ASAM_COMMON_MDF_V4-1-0/Base_Standard/Examples/Simple/test.mf4";
     group.sample_size(20);
-    group.bench_function("mdfr_with_mdf4", |b| b.iter(|| mdfinfo(&file_name)));
+    group.bench_function("mdfr_with_mdf4", |b| b.iter(|| mdfreader(&file_name)));
     group.finish();
     println!("mdfreader.mdfinfo\n");
     python_launch();
