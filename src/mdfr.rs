301d43c2d326139bed651a8aab8bdb87f00da27c
«ratal» <ratal@ratal.org>
2021-08-12 18:25:30 +0200

    added get_channel_data method and __repr__

--- src/mdfr.rs
+++ src/mdfr.rs
@@ -1,6 +1,7 @@
-use crate::mdfinfo::MdfInfo;
-use crate::mdfreader;
+use crate::{mdfreader, mdfinfo::MdfInfo};
 use pyo3::prelude::*;
+use pyo3::PyObjectProtocol;
+use numpy::IntoPyArray;
 
 #[pyclass]
 struct Mdf(MdfInfo);
@@ -15,5 +16,48 @@ impl Mdf {
     #[new]
     fn new(file_name: &str) -> Self {
         Mdf(mdfreader::mdfreader(file_name))
+    }
+    fn get_channel_data(&self, channel_name: String) {
+        let Mdf(mdf) = self;
+        match mdf {
+            MdfInfo::V3(_mdfinfo3) => {},
+            MdfInfo::V4(mdfinfo4) => {
+                mdfinfo4.get_channel_data(&channel_name);
+            }
+        }
+    }
+}
+
+#[pyproto]
+impl PyObjectProtocol for Mdf {
+    fn __repr__(&self) -> PyResult<String>   {
+        let mut output: String;
+        match &self.0 {
+            MdfInfo::V3(mdfinfo3) => {
+                output = format!("Version : {}\n", mdfinfo3.ver);
+                output.push_str(&format!("Version : {:?}\n", mdfinfo3.hdblock));
+            }
+            MdfInfo::V4(mdfinfo4) => {
+                output = format!("Version : {}\n", mdfinfo4.ver);
+                output.push_str(&format!("{}\n", mdfinfo4.hd_block));
+                let comments = &mdfinfo4.hd_comment;
+                for c in comments.iter() {
+                    output.push_str(&format!("{} {}", c.0, c.1));
+                }
+                for (master, list) in mdfinfo4.db.master_channel_list.iter() {
+                    output.push_str(&format!("\nMaster: {}\n", master));
+                    for channel in list.iter() {
+                        if let Some(data ) = mdfinfo4.get_channel_data(channel) {
+                            let data_min_max = data.min_max();
+                            output.push_str(&format!(" {} {}\n", channel, data_min_max));
+                        } else {
+                            output.push_str(&format!(" {} \n", channel));
+                        }
+                    }
+                }
+                output.push_str(&format!("\n"));
+            }
+        }
+        Ok(output)
     }
 }
