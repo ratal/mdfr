fffb267c120e1591d3cda2ce9049c9e28024184e
«ratal» <ratal@ratal.org>
2021-10-07 21:54:31 +0200

    reserved string capacity for UTF16 and SBC fro proper decoding

--- src/mdfreader/channel_data.rs
+++ src/mdfreader/channel_data.rs
@@ -123,13 +123,21 @@ impl ChannelData {
                     ChannelData::Complex64(Array1::<Complex<f64>>::zeros(cycle_count as usize))
                 }
                 ChannelData::StringSBC(_) => {
-                    ChannelData::StringSBC(vec![String::new(); cycle_count as usize])
+                    let mut target_vect: Vec<String> = vec![String::with_capacity(n_bytes as usize);  cycle_count as usize];
+                    for i in 0..cycle_count as usize {
+                        target_vect[i].reserve(n_bytes as usize);
+                    }
+                    ChannelData::StringSBC(target_vect)
                 }
                 ChannelData::StringUTF8(_) => {
-                    ChannelData::StringUTF8(vec![String::new(); cycle_count as usize])
+                    ChannelData::StringUTF8(vec![String::with_capacity(n_bytes as usize); cycle_count as usize])
                 }
                 ChannelData::StringUTF16(_) => {
-                    ChannelData::StringUTF16(vec![String::new(); cycle_count as usize])
+                    let mut target_vect: Vec<String> = vec![String::with_capacity(n_bytes as usize);  cycle_count as usize];
+                    for i in 0..cycle_count as usize {
+                        target_vect[i].reserve(n_bytes as usize);
+                    }
+                    ChannelData::StringUTF16(target_vect)
                 }
                 ChannelData::ByteArray(_) => {
                     ChannelData::ByteArray(vec![0u8; (n_bytes as u64 * cycle_count) as usize])
