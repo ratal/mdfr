3eaec4d06a35f8bb46a31531534eca3c32e2a743
«ratal» <ratal@ratal.org>
2022-02-03 00:59:03 +0100

    fixed bug with clap args

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod mdfwriter;
 
 fn main() -> io::Result<()> {
     let matches = App::new("mdfr")
+        .bin_name("mdfr")
         .version("0.1.0")
         .author("Aymeric Rateau <aymeric.rateau@gmail.com>")
         .about("reads ASAM mdf file")
@@ -16,24 +17,19 @@ fn main() -> io::Result<()> {
             Arg::new("file")
                 .help("Sets the input file to use")
                 .required(true)
+                .takes_value(true)
                 .index(1),
         )
         .arg(
-            Arg::new("v")
-                .multiple_values(true)
+            Arg::new("verbose")
+                .long("verbose")
+                .short('v')
+                .takes_value(true)
                 .help("Sets the level of verbosity"),
         )
         .get_matches();
 
     let file_name = matches.value_of("file").expect("File name missing");
-
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Printing normally...");
-        }
-    }
 
     //let info = mdfinfo::mdfinfo(file_name);
     mdfreader::mdfreader(file_name);
