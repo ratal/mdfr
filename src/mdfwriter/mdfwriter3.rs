8a3b7d492d847fd7f304bed4cbbd57bc404e5263
«ratal» <ratal@ratal.org>
2024-03-10 15:16:51 +0100

    fixed bug in shape calculation for mdf3

--- src/mdfwriter/mdfwriter3.rs
+++ src/mdfwriter/mdfwriter3.rs
@@ -39,7 +39,7 @@ pub fn convert3to4(mdf3: &MdfInfo3, file_name: &str) -> Result<MdfInfo4, Error> 
                                     bit_count: bit_count as u32,
                                     byte_count: cn.n_bytes as u32,
                                     ndim: 1,
-                                    shape: (vec![cycle_count; 1], Order::RowMajor),
+                                    shape: (vec![1], Order::RowMajor),
                                 };
                                 let master_signature = MasterSignature {
                                     master_channel: cg.master_channel_name.clone(),
@@ -76,7 +76,7 @@ pub fn convert3to4(mdf3: &MdfInfo3, file_name: &str) -> Result<MdfInfo4, Error> 
                                 bit_count: bit_count as u32,
                                 byte_count: cn.n_bytes as u32,
                                 ndim: 1,
-                                shape: (vec![cycle_count; 1], Order::RowMajor),
+                                shape: (vec![1], Order::RowMajor),
                             };
                             let master_signature = MasterSignature {
                                 master_channel: cg.master_channel_name.clone(),
